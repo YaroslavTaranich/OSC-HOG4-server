6e42e4e468a9287c0f49d195b321eb937d5b5077
Yaroslav Taranich <ytaranich@alfabank.ru>
2025-12-24 11:56:15 +0300

    add root package.json

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
-import './styles/variables.module.css';
-import './styles/base.module.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
